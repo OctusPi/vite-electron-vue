3aaf1106cbbbe7cb5c6d7bdf84e2a36b159cf40b
octuspi <octuspi@gmail.com>
2024-04-08 15:15:02 -0300

    Make template initial to build

---
+++ main.js
@@ -0,0 +1,37 @@
+const { app, BrowserWindow } = require('electron/main')
+const path = require('node:path')
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  win.loadFile('dist/index.html')
+
+  win.once('ready-to-show', () => {
+    // Maximiza a janela
+    win.maximize()
+    // Exibe a janela
+    win.show()
+  })
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
